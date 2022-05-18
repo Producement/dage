5e045ac5a36262468cfdad2c59a1c0893968e8e4
Maido Käära <maido@producement.com>
2022-05-18 12:54:16 +0300

    New Dart project

---
+++ bin/dage.dart
@@ -0,0 +1,5 @@
+import 'package:dage/dage.dart' as dage;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dage.calculate()}!');
+}
---
+++ test/dage_test.dart
@@ -0,0 +1,8 @@
+import 'package:dage/dage.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
